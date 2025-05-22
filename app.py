6b517a307c3ac69d08e27949ec4bbe272f1c7536
ghanmywiem <wiem.ghanmi@isimg.tn>
2025-05-23 01:09:24 +0200

    dd rapidfuzz to requirements and fix port binding

--- app.py
+++ app.py
@@ -109,4 +109,6 @@ def get_video():
     return jsonify({"videos": full_video_urls})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
